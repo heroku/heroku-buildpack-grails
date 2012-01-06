0b5920c0fd2914ce851d859e70bead7d6bcdbe21
Ryan Brainard <brainard@heroku.com>
2012-01-06 10:54:42 -0800

    add detect and release grails unit tests

---
+++ test/detect_test.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+. ${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh
+
+testDetect()
+{
+  mkdir -p ${BUILD_DIR}/grails-app
+  
+  capture ${BUILDPACK_HOME}/bin/detect ${BUILD_DIR}
+  
+  assertEquals 0 ${rtrn}
+  assertEquals "Grails" "$(cat ${STD_OUT})"
+  assertNull "$(cat ${STD_ERR})"
+}
+
+testNoDetectGrailsAppFileInsteadOfDirectory()
+{
+  touch ${BUILD_DIR}/grails-app
+
+  capture ${BUILDPACK_HOME}/bin/detect ${BUILD_DIR}
+ 
+  assertEquals 1 ${rtrn}
+  assertEquals "no" "$(cat ${STD_OUT})"
+  assertNull "$(cat ${STD_ERR})"
+}
---
+++ test/release_test.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+. ${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh
+
+testRelease()
+{
+  expected_release_output=`cat <<EOF
+---
+config_vars:
+  JAVA_OPTS: -Xmx384m -Xss512k -XX:+UseCompressedOops
+addons:
+  shared-database:5mb
+
+default_process_types:
+  web:      java \\$JAVA_OPTS -jar server/jetty-runner.jar --port \\$PORT target/*.war 
+
+EOF`
+
+  capture ${BUILDPACK_HOME}/bin/release ${BUILD_DIR}
+  assertEquals 0 ${rtrn}
+  assertEquals "${expected_release_output}" "$(cat ${STD_OUT})"
+  assertEquals "" "$(cat ${STD_ERR})"
+}
