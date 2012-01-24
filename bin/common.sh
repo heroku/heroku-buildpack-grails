a7704db8ce3d74c6f8a8a5ce67d2b3a9207b8860
Ryan Brainard <brainard@heroku.com>
2012-01-23 16:47:55 -0800

    add basic test for get property

--- bin/common.sh
+++ bin/common.sh
@@ -2,8 +2,8 @@
 
 get_property()
 {
-  local file=$1
-  local key=$(echo $2 | sed "s/\./\\\./g")
+  local file=${1?"No file specified"}
+  local key=$(echo ${2?"No key specified"} | sed "s/\./\\\./g")
 
-  grep ^$key $file | sed -E -e "s/$key[ \t]*=[ \t]*([^ \t]+)[ \t]*$/\1/g"
+  [ -f $file ] && grep ^$key $file | sed -E -e "s/$key[ \t]*=[ \t]*([^ \t]+)[ \t]*$/\1/g"
 }
