4106aaa5c255fa262368122795b0665c26e2fcec
Ryan Brainard <brainard@heroku.com>
2012-02-02 13:30:09 -0800

    break out escaping of key from escaping

--- bin/common.sh
+++ bin/common.sh
@@ -3,7 +3,10 @@
 get_property()
 {
   local file=${1?"No file specified"}
-  local key=$(echo ${2?"No key specified"} | sed "s/\./\\\./g")
+  local key=${2?"No key specified"}
+ 
+  # escape for regex
+  local escaped_key=$(echo $key | sed "s/\./\\\./g")
 
-  [ -f $file ] && grep ^$key $file | sed -E -e "s/$key[ \t]*=[ \t]*([A-Za-z0-9\.-]*).*/\1/g"
+  [ -f $file ] && grep ^$escaped_key $file | sed -E -e "s/$escaped_key[ \t]*=[ \t]*([A-Za-z0-9\.-]*).*/\1/g"
 }
